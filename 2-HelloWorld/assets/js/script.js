67fd52127a9adbec8db13b1d660c77ac5d68e32f
RodriguesFundo <rodriguesdomingosf@gmail.com>
2022-09-02 21:13:52 -0700

    Hello world updated
    I add a new function changeColor()

--- 2-HelloWorld/assets/js/script.js
+++ 2-HelloWorld/assets/js/script.js
@@ -1,5 +1,5 @@
 const screen = document.getElementById("main");
-//container.addEventListener('click', a=>alert(""))
+const th1= document.getElementById("h1");
 const space = document.getElementsByTagName("h1")['h1']
 const text= ".<Hello world/>";
 const sleep=250;
@@ -20,13 +20,23 @@ function writer(space, text, sleep){
     }, sleep);
 
 }
+function changeColor(back,fore){
+    document.body.style.backgroundColor=back;
+    th1.style.color= fore;
+}
 
-function changeColor(){
-    console.log("main")
-    screen.classList.add("light");
+
+function controler(){
     
+    if (color=="light") {
+        changeColor('#121212','#0de218');
+        color= "dark"   
+    } else if (color="dark") {
+        changeColor('#fff','#b00de2');
+        color="light"   
+    }
 }
 
 document.body.onload= writer(space, text, sleep);
 
-screen.addEventListener('click', changeColor);
+screen.addEventListener('click', controler);
